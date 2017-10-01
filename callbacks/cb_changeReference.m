bb47b64bc6a5165176756dd7c790f671badd53bb
Alex Cani <alexcani109@gmail.com>
2017-10-01 19:47:44 -0300

    Versão p entrega 1

---
+++ callbacks/cb_changeReference.m
@@ -0,0 +1,23 @@
+% Universidade Federal de Santa Catarina
+% Centro Tecnológico
+% Departamento de Automação e Sistemas
+% Autores: Alex Amadeu Cani
+%          Gabriel Paiva
+%          Iago de Oliveira Silvestre
+%          Luís Artur Kretzer Tavares Sobral
+% Ano/Semestre: 2017-2
+% 
+% Disciplina: Projeto Integrador (DAS5104)
+% Kit Didático para Controle
+
+function cb_changeReference(hObject,~)
+global ad;
+
+conversion = str2num(hObject.String);
+if isempty(conversion)
+    hObject.String = num2str(ad.referencia);
+else
+    ad.referencia = conversion;
+end
+
+end
